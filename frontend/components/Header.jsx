1a25705f315739e7d9226f3e7744501fb74c3ea5
iamali123 <iamali62426@gmail.com>
2023-03-21 12:58:02 +0400

    Header footer added

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import Wrapper from "./Wrapper";
+import Menu from "./Menu";
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [showCatMenu, setShowCatMenu] = useState(false);
+  const [show, setShow] = useState("translate-y-0");
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [categories, setCategories] = useState(null);
 
-export default Header
+  return (
+    <header
+      className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0 transition-transform duration-300 ${show}`}
+    >
+      <Wrapper className="h-[60px] flex justify-between items-center">
+        <Link href="/">
+          <img src="/logo.svg" className="w-[40px] md:w-[60px]" />
+        </Link>
+        <Menu/>
+      </Wrapper>
+    </header>
+  );
+};
+
+export default Header;
