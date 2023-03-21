9449f713bfef0c6c9feca9d99d95344c479c0b4c
iamali123 <iamali62426@gmail.com>
2023-03-21 14:56:14 +0400

    cart menu added

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import Wrapper from "./Wrapper";
 import Menu from "./Menu";
+import { IoMdHeartEmpty } from "react-icons/io";
+import { BsCart } from "react-icons/bs";
+import { BiMenuAltRight } from "react-icons/bi";
+import { VscChromeClose } from "react-icons/vsc";
 
 const Header = () => {
   const [mobileMenu, setMobileMenu] = useState(false);
@@ -18,7 +22,28 @@ const Header = () => {
         <Link href="/">
           <img src="/logo.svg" className="w-[40px] md:w-[60px]" />
         </Link>
-        <Menu/>
+        <Menu showCatMenu={showCatMenu} setShowCatMenu={setShowCatMenu} />
+        <div className="flex items-center gap-2 text-black">
+          {/* Icon start */}
+          <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
+            <IoMdHeartEmpty className="text-[19px] md:text-[24px]" />
+            <div className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">
+              51
+            </div>
+          </div>
+          {/* Icon end */}
+
+          {/* Icon start */}
+          <Link href="/cart">
+            <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
+              <BsCart className="text-[15px] md:text-[20px]" />
+              <div className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">
+                20
+              </div>
+            </div>
+          </Link>
+          {/* Icon end */}
+        </div>
       </Wrapper>
     </header>
   );
